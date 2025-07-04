4ccf6ab9180a6184fd662edd0b6596dbc2603e86
Lingchao Zhang <zhanglingchao2021@gmail.com>
2025-07-04 18:44:02 +1000

    Initial the project with Next.js: 1. Initialed the project with Next.js. 2. Implemented some fundamental themeing in tailwind.config.ts and globals.css. 3. added sources such as fonts, images and icons to the public folder. 4. replaced the default fonts with new one in the layout. 5. replaced default favicon with the new one.

---
+++ uni-lib-website/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const ibmPlexSans = localFont({
+  src: [
+    {path: "./public/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal"},
+    {path: "./public/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal"},
+    {path: "./public/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal"},
+    {path: "./public/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal"}
+  ]
+});
+
+const bebasNeue = localFont({
+  src: [
+    {path: "./public/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal"}
+  ],
+  variable: "--bebas-neue"
+});
+
+export const metadata: Metadata = {
+  title: "BookWise",
+  description: "a university library management system",
+};
+
+const RootLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout
